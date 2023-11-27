7d29f824e766d1eec515b8b15ccf76860b8c6838
Ruma303 <matteoh8@hotmail.it>
2023-11-27 19:51:11 +0100

    file di routing

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,21 +1,24 @@
-    const path = require('path');
-    function isAuth(req, res, next) {
-        const { user } = req.query;
-        const isAuthenticated = user === 'admin' ? true : user === 'staff' ? true : false;
-        if (isAuthenticated) {
-            req.user = user;
-            return next();
-        } else {
-            return res.state(302).redirect('/');
-        }
+const path = require('path');
+
+function isAuth(req, res, next) {
+    const { user } = req.query;
+    const isAuthenticated = user === 'admin' ? true : user === 'staff' ? true : false;
+    if (isAuthenticated) {
+        req.user = user;
+        return next();
+    } else {
+        return res.state(302).redirect('/');
     }
-    function permissions(req, res, next) {
-        const isAdmin = req.user === 'admin' ? true : false;
-        if (isAdmin) {
-            return res.sendFile(path.resolve('Admin', 'dashboard.html'));
-        } else {
-            return res.status(403).send('Accesso al back office come staff');
-        }
+}
+
+function permissions(req, res, next) {
+    const isAdmin = req.user === 'admin' ? true : false;
+    if (isAdmin) {
+        return res.sendFile(path.resolve('Admin', 'dashboard.html'));
+    } else {
+        return res.status(403).send('Accesso al back office come staff');
     }
-    module.exports = {isAuth, permissions};
+}
+
+module.exports = { isAuth, permissions };
 
