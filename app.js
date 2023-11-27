8bf9209137af81279ba5a53da40b3fa678988db9
Ruma303 <matteoh8@hotmail.it>
2023-11-27 19:35:07 +0100

    dashboard

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-    const express = require('express'), app = express(), morgan = require('morgan'),
-        path = require('path'), fs = require('fs'), helmet = require('helmet'),
-        PORT = process.env.PORT || 3000,
-        { isAuth, permissions } = require('./middleware/auth')
+const express = require('express'), app = express(), morgan = require('morgan'),
+    path = require('path'), fs = require('fs'), helmet = require('helmet'),
+    PORT = process.env.PORT || 3000,
+    { isAuth, permissions } = require('./middleware/auth')
 
-    app.listen(PORT);
-    app.use(morgan('dev'), helmet());
+app.listen(PORT);
+app.use(morgan('dev'), helmet());
 
-    app.get('/auth', isAuth, permissions);
-    app.get('/', (req, res) => res.send('Sei nella homepage'));
+app.get('/auth', isAuth, permissions);
+app.get('/', (req, res) => res.send('Sei nella homepage'));
--- middleware/auth.js
+++ middleware/auth.js
@@ -2,21 +2,19 @@ const path = require('path');
 
 function isAuth(req, res, next) {
     const { user } = req.query;
-    const isAuthenticated = user === 'admin' ? true :
-            user === 'staff' ? true : false;
+    const isAuthenticated = user === 'admin' ? true : user === 'staff' ? true : false;
     if (isAuthenticated) {
         req.user = user;
         return next();
     } else {
-        return res.state(302).redirect('/'); //# redirect
+        return res.state(302).redirect('/');
     }
 }
 
 function permissions(req, res, next) {
     const isAdmin = req.user === 'admin' ? true : false;
-
     if (isAdmin) {
-        return res.sendFile(path.resolve(__dirname, 'Admin', 'dashboard.html'));
+        return res.sendFile(path.resolve('Admin', 'dashboard.html'));
     } else {
         return res.status(403).send('Accesso al back office come staff');
     }
