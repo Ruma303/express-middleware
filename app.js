5add85971c2106b07792a25e3b6f1b526e47254e
Ruma303 <matteoh8@hotmail.it>
2023-11-29 22:02:32 +0100

    css

--- app.js
+++ app.js
@@ -6,7 +6,6 @@
 
 
     //% Middleware
-
     /* app.get('/', (req, res) => {
         console.log('Richiesta ricevuta');
         res.status(200).send('Risposta inviata\n');
@@ -14,24 +13,22 @@
     .get('/', (req, res) => {
         console.log('Richiesta ricevuta');
         res.status(200).send('Risposta inviata\n');
-    })
-    .listen(PORT); */
+    }); */
 
 
     //# next
     /* app.get('/', (req, res, next) => {
         console.log('Richiesta gestita dal primo middleware');
-        next()
+        next();
     })
     .get('/', (req, res) => {
         console.log('Richiesta ricevuta dal secondo middleware');
         res.status(200).send('Seconda risposta\n');
-    })
-    .listen(PORT); */
+    }); */
 
 
     //, Gestione dei middleware
-    /* app.get('/', first, second).listen(PORT);
+    /* app.get('/', first, second);
 
     function first(req, res, next) {
         console.log('Richiesta gestita dal primo middleware');
@@ -45,15 +42,17 @@
 
 
     //% Morgan
-    //const morgan = require('morgan');
-    //app.use(morgan('combined'));
+    /* const morgan = require('morgan');
+    //app.use(morgan('common'));
     //app.use(morgan('tiny'));
-
-    /* app.use(morgan('dev'));
+    app.use(morgan('dev'));
 
     app.get('/', (req, res) => {
         res.send('Richiesta ricevuta');
-    }).listen(3000) */
+    }).get('/admin', (req, res) => {
+        res.send('Richiesta ricevuta');
+    }) */
+
 
     //app.use('/admin', morgan('combined'));
 
@@ -64,7 +63,7 @@
 
 
     //% Middleware di Autenticazione ed Autorizzazione
-    /* const morgan = require('morgan');
+    const morgan = require('morgan');
     app.use(morgan('dev'));
 
     app.get('/auth', checkAuthentication, checkAuthorization);
@@ -72,64 +71,61 @@
 
     function checkAuthentication(req, res, next) {
         const { user } = req.query;
-        const isAuthenticated = user === 'admin' ? true :
-                user === 'staff' ? true : false;
+        const isAuthenticated = user === 'admin' ? true : user === 'staff' ? true : false;
         if (isAuthenticated) {
             req.user = user;
             return next();
         } else {
-            return res.status(401).send('Non puoi accedere a questa risorsa');
             //return res.redirect('/'); //# redirect
+            return res.status(401).send('Non puoi accedere a questa risorsa');
         }
     }
 
     function checkAuthorization(req, res, next) {
         const isAdmin = req.user === 'admin' ? true : false;
-
         if (isAdmin) {
-            return res.sendFile(path.resolve(__dirname, 'Admin', 'dashboard.html'));
+            return res.status(200).sendFile(path.resolve(__dirname, 'Admin', 'dashboard.html'));
         } else {
             return res.status(403).send('Accesso al back office come staff');
         }
-    } */
+    }
 
 
 
     //, Middleware globali
-    /* app.get('/user/1', checkAuthentication, checkAuthorization, (req, res) => console.log('Risposta: User 1'))
-        .get('/user/2', checkAuthentication, checkAuthorization, (req, res) => console.log('Risposta: User 2'))
-        .get('/user/3', checkAuthentication, checkAuthorization, (req, res) => console.log('Risposta: User 3'))
-    .listen(PORT); */
+    /* app
+    .get('/user/1', checkAuthentication, checkAuthorization, (req, res) => console.log('Risposta: User 1'))
+    .get('/user/2', checkAuthentication, checkAuthorization, (req, res) => console.log('Risposta: User 2'))
+    .get('/user/3', (req, res) => {console.log('Risposta: User 3'); res.send('sei in user 3')}); */
 
-     /* app.use(checkAuthentication, checkAuthorization);
+    //app.use(checkAuthentication, checkAuthorization);
 
-   app.get('/user/1', (req, res) => console.log('Risposta: User 1'))
+    /* app.get('/user/1', (req, res) => console.log('Risposta: User 1'))
         .get('/user/2', (req, res) => console.log('Risposta: User 2'))
         .get('/user/3', (req, res) => console.log('Risposta: User 3'))
-        .get('/no-global-middleware', specificMiddleware)
-    .listen(PORT);
+        .get('/no-global-middleware', specificMiddleware);
 
     function specificMiddleware(req, res) {
         console.log('Middleware attivato solo su questa rotta');
-    }*/
+    } */
 
 
     //# Middleware su gruppi di rotte
     /* app.use(['/user', '/admin'], checkAuthentication, checkAuthorization)
         .get('/user/:id', (req, res) => res.send('Rotta con middleware'))
-        .get('/risorsa', (req, res) => res.send('Ecco la risorsa'))
-        .listen(PORT); */
+        .get('/risorsa', (req, res) => res.send('Ecco la risorsa')); */
 
 
 
     //, Fallback
     /* app.get('*', (req, res) => {
         res.status(404).send('Pagina non trovata');
-    }); */
+    });
 
-    /* app.get('/', (req, res) => {
+    app.get('/', (req, res) => {
         res.status(200).send('Homepage');
     }); */
+
 
 
     //% Middleware built-in
@@ -155,4 +151,4 @@
 
 
     //# Cartella virtuale
-    //app.use('/assets', express.static('public'));
+    //app.use('/assets', express.static(path.join(__dirname, 'public')));
